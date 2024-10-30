7958ac9bfa465db938863010146d218e8ca52c5d
MeshackYaro <miishaqhyaro@gmail.com>
2024-10-30 15:02:25 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/yasame/scriptapp/FirstScriptApplication.java
@@ -0,0 +1,13 @@
+package com.yasame.scriptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstScriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasame/scriptapp/FirstScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasame.scriptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
