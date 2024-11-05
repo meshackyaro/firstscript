07d3e6d6ac4c3d1d58389d91d78ed6360656abfb
MeshackYaro <yaromeshack@gmail.com>
2024-11-05 15:46:41 +0100

    fibonacci method added

--- src/main/java/com/yasame/scriptapp/controllers/UserController.java
+++ src/main/java/com/yasame/scriptapp/controllers/UserController.java
@@ -1,7 +1,22 @@
 package com.yasame.scriptapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
+    @GetMapping("/fibonacci/{n}")
+    public long calculateFibonacci(@PathVariable int n) {
+
+        return fibonacci(n);
+    }
+
+    private long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
